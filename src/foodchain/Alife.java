daa6a29162349f9e9eb555df0f249f6988c67abf
Ke-suke <suk_ch3.mdstw-mon@i.softbank.jp>
2014-06-18 18:36:23 +0900

    tes

--- src/foodchain/Alife.java
+++ src/foodchain/Alife.java
@@ -7,11 +7,35 @@ class Alife {
 		int width = 800;
 		int height = 800;
 		
-		Frame frame = new Frame("�H���A����z�肵���l�H����",width+20,height+50);
+		Frame frame = new Frame("食物連鎖を想定した人工生命",width+20,height+50);
 		frame.setVisible(true);
 		
-		World world = new World(width,height);
-		frame.add(world);
+		FrameAndField faf = new FrameAndField(width,height);
+		frame.add(faf);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		while(true){
+			try{
+				Thread.sleep(100);
+			}catch(Exception e){
+				System.out.println(e);
+			}
+			
+			
+			
+		}
 	}
-//s
 }
